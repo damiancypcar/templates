2d7bed0a2aa6b741cff58073e5c09e3b76bb2c50
Damian Cypcar <damiancypcar@gmail.com>
2023-06-15 22:17:36 +0200

    Add powershell template

---
+++ powershell.ps1
@@ -0,0 +1,9 @@
+# ----------------------------------------------------------
+# Author:          damiancypcar
+# Modified:        03.03.2023
+# Version:         1.0
+# Desc:            description
+# ----------------------------------------------------------
+# [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseApprovedVerbs')] Param()
+#   ^ - disable an unapproved verb. PSScriptAnalyzer notification
+
