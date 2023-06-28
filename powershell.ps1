770a5452613cacc55811d93e3966ac7d6b69d624
Damian Cypcar <damiancypcar@gmail.com>
2023-06-28 07:32:24 +0200

    Add yes/no to ps

--- powershell.ps1
+++ powershell.ps1
@@ -7,3 +7,13 @@
 # [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseApprovedVerbs')] Param()
 #   ^ - disable an unapproved verb. PSScriptAnalyzer notification
 
+# ROOT CHECK
+
+
+# YES/NO ASK
+$decision = $Host.UI.PromptForChoice('Confirm', 'Do you want to continue?', @("&Yes", "&No"), 1)
+if ($decision -eq 0) {
+    Write-Host 'Your choice is Yes.'
+} else {
+    Write-Host 'Your choice is No.'
+}
